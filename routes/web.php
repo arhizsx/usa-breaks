4310be99789c9f0166bc78c5a9e6f5cf46555585
arhizsx <arhizsx@gmail.com>
2024-08-30 15:18:45 +0800

    Laravel Setup

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,13 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/scheduled', function () {
+        return view('dashboard');
+    })->name('scheduled');
+
+    Route::get('/scraped', function () {
+        return view('dashboard');
+    })->name('scraped');
+
 });
