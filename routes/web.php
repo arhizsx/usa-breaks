2776be8a8f9ac3239ea0b6af696c553625657d38
arhizsx <arhizsx@gmail.com>
2024-08-30 15:21:56 +0800

    Laravel Setup

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
+
     Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
