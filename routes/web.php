eb1da172a2e2ed61b6118d57a87bd1cd5299e0f3
arhizsx <arhizsx@gmail.com>
2024-08-30 14:31:21 +0800

    Laravel Setup

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/', function () {
-        return view('welcome');
+        return view('dashboard')->name('dashboard');
     });
-});
-
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified',])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 });
