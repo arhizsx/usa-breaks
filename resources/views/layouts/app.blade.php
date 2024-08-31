d4468d98bddee3a1b33460bef1f629f20fd5876e
arhizsx <arhizsx@gmail.com>
2024-08-31 17:14:01 +0800

    Scraper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <script src="https://kit.fontawesome.com/65ee368307.js" crossorigin="anonymous"></script>
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/datagrid.js'])
 
