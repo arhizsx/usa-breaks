53d80734946b8073b9238643e659f3a0d10353e9
arhizsx <arhizsx@gmail.com>
2024-08-31 16:43:54 +0800

    Scraper

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -72,7 +72,7 @@ textarea {
         </div>
     </div>
 
-    <div class="py-12">
+    <div class="pb-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
