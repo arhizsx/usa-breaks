9bc33c80e576ccc631e0f76bf51148ea6e35a2dd
arhizsx <arhizsx@gmail.com>
2024-08-31 11:53:14 +0800

    Scraper

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@ function expandTextarea(id) {
     }, false);
 
     var lht = parseInt($('#certificate_numbers').css('lineHeight'),10);
-    var lines = $('#certificate_numbers').attr('scrollHeight') / lht;
+    var lines = parseInt($('#certificate_numbers').attr('scrollHeight') ) / lht;
     console.log(lines);
 }
 
