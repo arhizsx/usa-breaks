5ed453cedcb96b1ee8f31b4778e7c2d449bb715a
arhizsx <arhizsx@gmail.com>
2024-08-31 10:35:27 +0800

    Scraper

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="container-fluid">
+                <div class="container-fluid p-3">
                     <div class="row">
                         <div class="col-12">
                             <label for="filename" class="form-label">Filename</label>
