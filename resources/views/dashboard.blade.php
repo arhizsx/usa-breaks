c3901bda6ca3b72c1f6ad2cb46468b4147270762
arhizsx <arhizsx@gmail.com>
2024-08-31 16:42:41 +0800

    Scraper

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -72,6 +72,23 @@ textarea {
         </div>
     </div>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                <div class="container-fluid p-4">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div id="gridContainer"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
 </x-app-layout>
 <script>
 
