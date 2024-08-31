ee114381d0c4ec16fde3291cd8388be1eaec95c1
arhizsx <arhizsx@gmail.com>
2024-08-31 14:55:36 +0800

    Scraper

--- app/Models/Scraper.php
+++ app/Models/Scraper.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Scrape extends Model
+class Scraper extends Model
 {
     use HasFactory;
 
     protected $table = 'scrapes';
 
     protected $fillable = [
+        'id',
         'user_id',
         'certificate_number',
         'data',
