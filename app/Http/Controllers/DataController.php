6f4266dc088319f6e6ccf630efc20336c4e1fb0d
arhizsx <arhizsx@gmail.com>
2024-09-01 14:08:59 +0800

    Full

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 use App\Models\Scraper;
 use App\Models\Orders;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Certificate;
 
 class DataController extends Controller
 {
