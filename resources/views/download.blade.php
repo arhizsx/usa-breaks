a574c19b26f64ff5e81094fb80b85fc866e7e9fa
arhizsx <arhizsx@gmail.com>
2024-09-04 04:41:46 +0800

    Downloader

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,15 +1,12 @@
 <?php
 $order_id = $_GET['order_id'];
 $zip_filename = "images_$order_id.zip";
-
 $command = escapeshellcmd("python3 /home/arhizsx/download_images.py $order_id $zip_filename");
 $output = shell_exec($command);
-
 if (file_exists(trim($output))) {
     header('Content-Type: application/zip');
     header('Content-Disposition: attachment; filename="' . basename($output) . '"');
     header('Content-Length: ' . filesize($output));
-
     flush();
     readfile($output);
     unlink($output);  // Optionally, delete the file after download
