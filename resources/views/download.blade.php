9bcd9d74bca639b38bc0ee91167f300f5c9adc9a
arhizsx <arhizsx@gmail.com>
2024-09-04 04:47:17 +0800

    Downloader

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,6 +1,9 @@
 <?php
 $order_id = $_GET['order_id'];
-$zip_filename = "images_$order_id.zip";
+
+$order = DB::table("orders")->where("id", $order_id)->first();
+
+$zip_filename = "/var/www/scraper/public/files/" . $order->filename;
 $command = escapeshellcmd("python3 /home/arhizsx/download_images.py $order_id $zip_filename");
 $output = shell_exec($command);
 
