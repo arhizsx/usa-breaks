861a52cceeee8ed6c2ee0f83b8066c86a0382ba5
arhizsx <arhizsx@gmail.com>
2024-09-04 17:24:19 +0800

    Card Update API

--- public/download.php
+++ public/download.php
@@ -1,7 +1,7 @@
 <?php
 $order_id = 1;  // Example order ID
 $zip_filename = "/var/www/scraper/public/files/try.zip";
-$command = escapeshellcmd("python3 /va/www/scraper/download_images.py $order_id $zip_filename 2>&1");
+$command = escapeshellcmd("sudo python3 /va/www/scraper/download_images.py $order_id $zip_filename 2>&1");
 
 $output = [];
 $return_var = 0;
