425cf0cb8409486968cdd053f2a02a3fb9c13602
arhizsx <arhizsx@gmail.com>
2024-09-04 19:16:38 +0800

    Card Update API

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -2,7 +2,7 @@
 
 $order = DB::table("orders")->where("id", $order_id)->first();
 
-$zip_filename = $order->filename;
+$zip_filename = str_replace(" ", "_", $order->filename);
 
 
 $command = "python3 /var/www/scraper/download_images.py $order_id $zip_filename 2>&1"; // Capture both stdout and stderr
