1d7b9dacccdcda62a764b3db371e247dcde0e1b9
arhizsx <arhizsx@gmail.com>
2024-09-04 19:05:02 +0800

    Card Update API

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -89,7 +89,7 @@
         $(document).find(".card_table").addClass("d-none");
         $(document).find(".download_link").addClass("d-none");
         $(document).find(".loading").removeClass("d-none");
-        $(document).find(".download_zip_btn").data("order_id") = info.id;
+        $(document).find(".download_zip_btn").data("order_id", info.id);
 
         $.when( order_cards ).done( function( order_cards ){
 
