f4cb139e0c8199c4adde557413e62c5399162a11
arhizsx <arhizsx@gmail.com>
2024-09-04 17:34:14 +0800

    Card Update API

--- public/download.php
+++ public/download.php
@@ -1,7 +1,7 @@
 <?php
-$order_id = 1;  // Example order ID
+$order_id = 1;
 $zip_filename = "/var/www/scraper/public/files/try.zip";
-$command = escapeshellcmd("sudo python3 /home/arhizsx/download_images.py $order_id $zip_filename 2>&1");
+$command = "python3 /home/arhizsx/download_images.py $order_id $zip_filename 2>&1"; // Capture both stdout and stderr
 
 
 // Execute the Python script and capture output and errors
