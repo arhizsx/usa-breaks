e0eda49d549248a4a21aa2f59aee9dc1194307ae
arhizsx <arhizsx@gmail.com>
2024-09-04 19:14:27 +0800

    Card Update API

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -4,6 +4,8 @@ $order = DB::table("orders")->where("id", $order_id)->first();
 
 $zip_filename = $order->filename;
 
+dd($order);
+
 $command = "python3 /var/www/scraper/download_images.py $order_id $zip_filename 2>&1"; // Capture both stdout and stderr
 
 // Execute the Python script and capture output and errors
