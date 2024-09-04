6382ca8fce31ab65936490a8c975d477fc565443
arhizsx <arhizsx@gmail.com>
2024-09-04 15:11:24 +0800

    Card Update API

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'USA Breaks') }}</title>
+        <title>USA Breaks</title>
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
