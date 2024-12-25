7a32d8cb8fae5ed2818b22e8b6b91ba1b04bea5b
arhizsx <arhizsx@gmail.com>
2024-12-26 00:29:12 +0800

    Docker to HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\HttpFoundation\Request;
-use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,12 +20,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    // Trust all proxies
-    \Illuminate\Http\Request::setTrustedProxies(['**'], Request::HEADER_X_FORWARDED_ALL);
-
-    // Force HTTPS URLs
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
-    }
     }
 }
