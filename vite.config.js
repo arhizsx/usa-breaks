c34744f4697182cd2d260a648902df8ff81a931d
arhizsx <arhizsx@gmail.com>
2024-12-26 00:56:32 +0800

    Docker to HTTPS

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+
         laravel({
+            server: {
+                https: true,
+                hmr: {
+                    protocol: 'wss', // Use WebSocket Secure
+                },
+            },
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
