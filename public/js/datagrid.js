6ae7aebeaf4d2a10c0ea7d0afeb7397e9b57096f
arhizsx <arhizsx@gmail.com>
2024-12-25 23:39:23 +0800

    Scraper to Cloud Run

---
+++ public/js/datagrid.js
@@ -0,0 +1,87 @@
+
+$.fn.setDatagrid = function (modal, datasource, columns, callback = false) {
+
+    let whatmodal = modal;
+
+    $(this).dxDataGrid({
+        dataSource: datasource,
+        rowAlternationEnabled: true,
+        selection: {
+            mode: 'single',
+        },
+        width: '100%',
+        showBorders: true,
+        columnHidingEnabled: true,
+        columnAutoWidth: false,
+        export: {
+            enabled: true,
+        },
+        columnFixing: {
+            enabled: false
+        },
+        onContentReady: function (e) {
+        },
+        searchPanel: {
+            visible: true,
+            highlightCaseSensitive: true,
+        },
+        columnChooser: {
+            enabled: true,
+            mode: "select",
+            allowSearch: true,
+        },
+        scrolling: {
+            rowRenderingMode: 'standard',
+        },
+        paging: {
+            pageSize: 50,
+        },
+        pager: {
+            visible: true,
+            allowedPageSizes: [10, 20, 50, 'all'],
+            showPageSizeSelector: true,
+            showInfo: true,
+            showNavigationButtons: true,
+        },
+        columnsAutoWidth: true,
+        showBorders: true,
+        filterRow: {
+            visible: false,
+            applyFilter: 'auto',
+        },
+        headerFilter: {
+            visible: true,
+        },
+        onRowClick: function (e) {
+
+            $(this).openModal(whatmodal, e.data, callback);
+
+        },
+        allowColumnResizing: {
+            enabled: false
+        },
+        columns: columns,
+        showBorders: true,
+    });
+}
+
+
+$.fn.openModal = function (modal, data, callback) {
+
+    $(document).find(modal).modal("show");
+
+    $.each(data, function (k, v) {
+
+        $(modal).find("input[name='" + k + "']").val(v);
+        $(modal).find("textarea[name='" + k + "']").text(v);
+
+    });
+
+    $(modal).find(".btn-action").attr("data-id", data.id.toString());
+
+    if (callback != false) {
+        eval(callback + "(data)");
+    }
+
+};
+
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,9 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="css/app.css" rel="stylesheet" />
+        <link href="js/app.js" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
