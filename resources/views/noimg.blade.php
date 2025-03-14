b0e9315d0923bc146181afa6b0ec0b09b0a60bec
arhizsx <arhizsx@gmail.com>
2025-03-14 09:33:15 +0800

    Countdown Timer

--- resources/views/noimg.blade.php
+++ resources/views/noimg.blade.php
@@ -296,12 +296,13 @@
                 return;
             }
 
-            console.log("test");
 
             const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
             const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+            console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
 
             $("#countdown").text(`${days}d ${hours}h ${minutes}m ${seconds}s`);
         }
